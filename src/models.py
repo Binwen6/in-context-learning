87d6433c077acdf0f99148bad37f0ce4abbddccd
YQuan13 <156297313+YQuan13@users.noreply.github.com>
2025-04-15 09:07:50 -0700

    Update models.py

--- src/models.py
+++ src/models.py
@@ -71,6 +71,16 @@ def get_relevant_baselines(task_name):
             (XGBoostModel, {}),
             (AveragingModel, {}),
         ],
+        "gaussian_kernel_regression":[
+            (LeastSquaresModel, {}),
+            (NNModel, {"n_neighbors": 3}),
+            (AveragingModel, {})
+        ]
+        "nonlinear_dynamics":[
+            (LeastSquaresModel, {}),
+            (NNModel, {"n_neighbors": 3}),
+            (AveragingModel, {})
+        ]
     }
 
     models = [model_cls(**kwargs) for model_cls, kwargs in task_to_baselines[task_name]]
